e2ab8b3bdb3e6b1fd9641da1f476d2f4c75b778d
Ventzislav Shabansky <ventzislav.shabansky@delasport.com>
2021-02-02 20:02:56 +0200

    Add simple getters/setters to Product

--- classes/warehouse/Product.php
+++ classes/warehouse/Product.php
@@ -8,4 +8,41 @@ class Product
     private float $length;
     private float $width;
     private float $height;
+
+    public function __construct() {
+
+    }
+
+    public function setModel(string $model) :void{
+        $this->model = $model;
+    }
+
+    public function setLength(float $length) :void{
+        $this->length = $length;
+    }
+
+    public function setWidth(float $width) :void{
+        $this->width;
+    }
+
+    public function setHeight(float $height) :void{
+        $this->height;
+    }
+
+    public function getModel() :string{
+        return $this->model;
+    }
+
+    public function getLength() :float{
+        return $this->length;
+    }
+
+    public function getWidth() :float{
+        return $this->width;
+    }
+
+    public function getHeight() :float{
+        return $this->height;
+    }
+
 }
